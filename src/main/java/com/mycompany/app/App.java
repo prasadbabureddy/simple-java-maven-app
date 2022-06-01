5d76a02b5cdd144dbd03ed21b7b329eff8698a22
prasadbabureddy <106139240+prasadbabureddy@users.noreply.github.com>
2022-06-01 05:54:31 +0000

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello prasad redyy!";
+    private final String message = "Hello world!";
 
     public App() {}
 
